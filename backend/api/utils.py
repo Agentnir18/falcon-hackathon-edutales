494850f47cb5802c6e1618dc7320a8e4c721ee5e
Jerom <jeromjomanthara@gmail.com>
2024-08-03 22:03:04 +0530

    feat(backend): add custom response

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -1,5 +1,32 @@
 from ai71 import AI71
 from decouple import config
+from rest_framework.response import Response
+from rest_framework import status
+
+class CustomResponse():
+    def __init__(self, message=None, data=None):
+        self.message = {} if message is None else message
+        self.data = {} if data is None else data
+
+    def success_response(self):
+        self.error = False
+        self.status_code = status.HTTP_200_OK
+        return Response({
+            "error": self.error, 
+            "message": self.message, 
+            "data": self.data, 
+            "status_code": self.status_code
+            }, status=self.status_code)
+    
+    def failure_response(self):
+        self.error = True
+        self.status_code = status.HTTP_400_BAD_REQUEST
+        return Response({
+            "error": self.error, 
+            "message": self.message, 
+            "data": self.data, 
+            "status_code": self.status_code
+            }, status=self.status_code)
 
 AI71_API_KEY = config("AI71_API_KEY")
 
