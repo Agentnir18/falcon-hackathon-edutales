e7ff59797b2e2ff5ad31c54db1483b6ebe27a50e
Jerom <jeromjomanthara@gmail.com>
2024-08-05 00:26:17 +0530

    feat(backend): add end point to be targetted by cron jobs to keep site active

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path("generate-story/", views.GenerateStory.as_view(), name="generate-story"), # post
     path("sitcom-explain/", views.SitcomExplain.as_view(), name="sitcom-explain"), # post
+
+    path("run/", views.Runner.as_view(), name="run"), # get
     
 ]
